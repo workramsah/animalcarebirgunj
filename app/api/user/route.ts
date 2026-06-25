3a34a9379dc826f20eef4d55b1ed0339eabb4ac4
workramsah <workramsah@gmail.com>
2026-06-25 06:46:36 +0545

    try fix error 1

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,6 +2,10 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(){
-    const ram = await prisma.user.findMany();
-    return NextResponse.json(ram)
+    try {
+        const users = await prisma.user.findMany();
+        return NextResponse.json(users)
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to fetch users" }, { status: 500 })
+    }
 }
